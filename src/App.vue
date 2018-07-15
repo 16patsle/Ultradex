f2292bd215c929e45e2d36841b581befd349cf41
Patrick Sletvold <patricksletvold@hotmail.com>
2018-07-15 16:37:36 +0200

    Reshuffle layout and css in main App file

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <div class="columns">
-    <aside class="sidebar-view column is-one-third is-narrow-tablet is-one-quarter-widescreen">
-      <router-view name="sidebar"></router-view>
-    </aside>
-    <main class="main-view column">
-      <router-view name="main"></router-view>
-    </main>
+      <aside class="sidebar-view column is-one-third is-narrow-tablet is-one-quarter-widescreen">
+        <router-view name="sidebar"></router-view>
+      </aside>
+      <div class="column main-container">
+        <nav id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </nav>
+        <main class="main-view">
+          <router-view name="main"></router-view>
+        </main>
+      </div>
     </div>
-    
   </div>
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  width: 100vw;
+  height: 100vh;
 }
+
+#app {
+  height: 100%;
+  width: 100%;
+  overflow-y: hidden;
+  text-align: center;
+}
+
+#app .columns {
+  height: 100%;
+  width: 100%;
+}
+
+aside.sidebar-view {
+  height: 100vh;
+  overflow-y: scroll;
+  margin-left: 0.75rem;
+  margin-right: 0.75rem;
+  margin-top: 0.75rem;
+}
+
 #nav {
   padding: 30px;
 }
