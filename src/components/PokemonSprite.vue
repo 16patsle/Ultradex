8c819f7412868a1149885829ca83e6b3a622c5e1
Patrick Sletvold <patricksletvold@hotmail.com>
2018-07-16 15:51:59 +0200

    Rework Sprite component to improve styling when the image load failed

--- src/components/PokemonSprite.vue
+++ src/components/PokemonSprite.vue
@@ -1,5 +1,7 @@
 <template>
-  <img :src="sprite" :alt="'Default ' + spriteName + ' sprite for ' + pokemonName">
+  <div class="sprite-wrapper">
+    <img :src="sprite" :alt="spriteName + ' sprite for ' + pokemonName | capitalize">
+  </div>
 </template>
 
 <script>
@@ -15,8 +17,37 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+div.sprite-wrapper {
+  position: relative;
+  display: inline-block;
+  width: 96px;
+  height: 96px;
+  margin: 0 auto;
+}
+
 img {
   width: 96px;
   height: 96px;
+}
+
+img:after {
+  content: attr(alt);
+
+  font-size: 12px;
+  color: rgb(100, 100, 100);
+  line-height: 2;
+
+  display: block;
+  position: absolute;
+  width: 96px;
+  height: 96px;
+  left: 50%;
+  margin-left: -48px;
+  top: 50%;
+  margin-top: -48px;
+  padding-top: 15px;
+  z-index: 2;
+  background-color: #eee;
+  border: 2px solid white;
 }
 </style>
