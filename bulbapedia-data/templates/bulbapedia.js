d01e59d782fcaabfddcc2619c4032924a871fca4
Patrick Sletvold <patricksletvold@hotmail.com>
2018-07-19 17:44:09 +0200

    Add a bunch more link templates

--- bulbapedia-data/templates/bulbapedia.js
+++ bulbapedia-data/templates/bulbapedia.js
@@ -26,6 +26,13 @@ const bulbapedia = {
     const s = obj.data[1] || "";
     return linkify(`Pokémon ${name} Version${s}`, `Pokemon ${name}`);
   },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:P
+  p: tmpl => {
+    const obj = pipeList(tmpl);
+    const pageName = obj.data[0] || "";
+    const displayName = obj.data[1] || pageName;
+    return linkify(pageName + " (Pokémon)", displayName);
+  },
   //https://bulbapedia.bulbagarden.net/wiki/Template:M
   m: tmpl => {
     return linkTemplate(tmpl, "", " (move)");
@@ -44,6 +51,32 @@ const bulbapedia = {
     const name = obj.data[1] || "";
     const id = obj.data[2] || "";
     return linkify(`${name} (${tfg} ${id})`, name);
+  },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:An
+  an: tmpl => {
+    return linkTemplate(tmpl, "", " (anime)");
+  },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:TP
+  tp: tmpl => {
+    const obj = pipeList(tmpl);
+    const trainer = obj.data[0] || "";
+    const pokemon = obj.data[1] || "";
+    return linkify(`${trainer}'s ${pokemon}`, pokemon);
+  },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:Pkmn
+  pkmn: tmpl => {
+    return linkTemplate(tmpl, "Pokémon ");
+  },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:OBP
+  obp: tmpl => {
+    const obj = pipeList(tmpl);
+    const title = obj.data[0] || "";
+    const parenthetical = obj.data[1] || "";
+    return linkify(`${title} (${parenthetical})`, title);
+  },
+  //https://bulbapedia.bulbagarden.net/wiki/Template:Adv
+  adv: tmpl => {
+    return linkTemplate(tmpl, "", " (Adventures)");
   }
 };
 
