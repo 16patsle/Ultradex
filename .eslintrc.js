ebd3e6daea780cc3f07b7238abf497af33eef2e2
Patrick Sletvold <patricksletvold@hotmail.com>
2018-11-25 11:42:45 +0100

    Lint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/require-default-prop': 'error',
-    'vue/require-prop-types': 'error',
-    'vue/attributes-order': 'error',
-    'vue/order-in-components': 'error'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/require-default-prop": "error",
+    "vue/require-prop-types": "error",
+    "vue/attributes-order": "error",
+    "vue/order-in-components": "error"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
