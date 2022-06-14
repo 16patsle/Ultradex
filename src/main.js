968d7605fe6b41e654c4df2952158a44c29e69bc
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-14 15:40:50 +0200

    Upgrade oruga to Vue 3 version

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import "./registerServiceWorker";
-import Oruga from "@oruga-ui/oruga";
+import Oruga from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
 import "@oruga-ui/theme-bulma/dist/bulma.css";
 import { createPinia } from "pinia";
