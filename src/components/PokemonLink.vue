cbf858f8d3ddeba150e2ab9746aa0d2df4f5de12
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-17 18:36:30 +0200

    Load default pokemon variety data in PokemonList to display icon

--- src/components/PokemonLink.vue
+++ src/components/PokemonLink.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="link-wrapper is-flex is-align-items-center">
-    <PokemonSpriteIcon v-if="defaultVariety" :pokemonData="defaultVariety" />
+    <PokemonSpriteIcon v-if="variety" :pokemonData="variety" />
     <div>
       <router-link :to="'/pokemon/' + pokemonId">{{ pokemonName }}</router-link>
     </div>
@@ -8,17 +8,21 @@
 </template>
 
 <script setup lang="ts">
+import type { NamedAPIResource } from "@/types/PokeApi";
+import { handleError } from "@/utils/handleError";
 import { idFromUrl } from "@/utils/idFromUrl";
-import { computed } from "vue";
+import { computed, ref, watch } from "vue";
 import { usePokemonStore } from "../stores/pokemonStore";
 import { pokemonNameLocalized } from "../utils/pokemonNameLocalized";
 import PokemonSpriteIcon from "./PokemonSpriteIcon.vue";
 
-const props = defineProps({
-  pokemon: { type: Object, required: true },
-});
+const props = defineProps<{
+  pokemon: NamedAPIResource;
+}>();
 
 const store = usePokemonStore();
+
+const error = ref("");
 
 const pokemonId = computed(() => idFromUrl(props.pokemon.url));
 const pokemonName = computed(() => {
@@ -29,9 +33,35 @@ const pokemonName = computed(() => {
   const name = store.pokemonList[pokemonId.value - 1]?.name;
   return name ? name.charAt(0).toUpperCase() + name.slice(1) : "";
 });
-const defaultVariety = computed(() =>
-  store.findDefaultVariety(pokemonId.value)
-);
+const varietyId = ref<number | undefined>(undefined);
+
+const variety = computed(() => {
+  if (varietyId.value) {
+    return store.pokemonVarieties[varietyId.value];
+  } else {
+    return undefined;
+  }
+});
+
+const fetchPokemonVariety = async () => {
+  if (!variety.value) {
+    error.value = "";
+
+    try {
+      requestIdleCallback(async () => {
+        varietyId.value = await store.fetchDefaultPokemonVariety(
+          pokemonId.value
+        );
+      });
+    } catch (err) {
+      error.value = handleError(err);
+    }
+  }
+};
+
+watch(props, fetchPokemonVariety);
+
+fetchPokemonVariety();
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
