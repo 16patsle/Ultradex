7244b18c1a7b80459673b6bed57130dacc5afb1e
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-17 19:20:38 +0200

    Create generic PokemonIcon component

---
+++ src/components/PokemonIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="type">
+    <img :src="src" :alt="alt" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  alt: string;
+  type: "icon" | "default";
+}>();
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.icon,
+.icon img {
+  width: 40px;
+  height: 30px;
+}
+
+.default,
+.default img {
+  width: 30px;
+  height: 30px;
+}
+
+img {
+  image-rendering: pixelated;
+  image-rendering: crisp-edges;
+}
+</style>
--- src/components/PokemonSpriteIcon.vue
+++ src/components/PokemonSpriteIcon.vue
@@ -1,50 +1,36 @@
 <template>
-  <div v-if="iconData.src" :class="iconData.type">
-    <img :src="iconData.src" alt="" />
-  </div>
+  <PokemonIcon
+    v-if="iconData.src"
+    :src="iconData.src"
+    alt=""
+    :type="iconData.type"
+  />
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import PokemonIcon from "./PokemonIcon.vue";
 import type { Pokemon } from "@/types/Pokemon";
 
 const props = defineProps<{
   pokemonData: Pokemon;
 }>();
 
-const iconData = computed(() => {
-  const icon =
-    props.pokemonData?.sprites.versions["generation-vii"].icons.front_default;
-  if (icon) {
-    return {
-      src: icon,
-      type: "icon",
-    };
-  } else {
-    return {
-      src: props.pokemonData?.sprites.front_default,
-      type: "default",
-    };
+const iconData = computed(
+  (): { src: string | null; type: "icon" | "default" } => {
+    const icon =
+      props.pokemonData?.sprites.versions["generation-vii"].icons.front_default;
+    if (icon) {
+      return {
+        src: icon,
+        type: "icon",
+      };
+    } else {
+      return {
+        src: props.pokemonData?.sprites.front_default,
+        type: "default",
+      };
+    }
   }
-});
+);
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.icon,
-.icon img {
-  width: 40px;
-  height: 30px;
-}
-
-.default,
-.default img {
-  width: 30px;
-  height: 30px;
-}
-
-img {
-  image-rendering: pixelated;
-  image-rendering: crisp-edges;
-}
-</style>
