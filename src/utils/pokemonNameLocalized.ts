f944454956f9d328cab67bcc9ae1f30af830464f
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-20 10:52:21 +0200

    Add types for pokemonNameLocalized and fix for varieties

--- src/utils/pokemonNameLocalized.ts
+++ src/utils/pokemonNameLocalized.ts
@@ -1,6 +1,9 @@
+import type { PokemonSpecies, Pokemon } from "@16patsle/pokeapi.js";
+import { usePokemonStore } from "@/stores/pokemonStore";
 import { capitalize } from "@/utils/capitalize";
+import { idFromUrl } from "./idFromUrl";
 
-export const pokemonNameLocalized = (pokemonSpecies) => {
+export const pokemonNameLocalized = (pokemonSpecies: PokemonSpecies) => {
   for (const name of pokemonSpecies.names) {
     if (name.language.name === "en") {
       return name.name;
@@ -9,18 +12,25 @@ export const pokemonNameLocalized = (pokemonSpecies) => {
   return pokemonSpecies.name;
 };
 
-export const pokemonNameLocalizedVariety = (pokemonSpecies, pokemonVariety) => {
+export const pokemonNameLocalizedVariety = (pokemonSpecies: PokemonSpecies, pokemonVariety: Pokemon) => {
   const localizedName = pokemonNameLocalized(pokemonSpecies);
   if (pokemonVariety.name === pokemonSpecies.name) {
     // Default variety, use the species name
     return localizedName;
   } else {
+    const store = usePokemonStore();
     // Other variety, try finding the name in the form data
-    const form = pokemonVariety.forms.find((form) => form.data?.is_default);
-    if (form) {
+    const form = pokemonVariety.forms.find((form) => {
+      const formId = idFromUrl(form?.url);
+      return formId ? store.pokemonForms[formId]?.is_default : false;
+    });
+    
+    const formId = idFromUrl(form?.url);
+    if (formId) {
+      const formData = store.pokemonForms[formId]
       // The .names array is preferred, but not always available, fall back on .form_names
-      const name = form.data.names.find((name) => name.language.name === "en");
-      const formName = form.data.form_names.find(
+      const name = formData.names.find((name) => name.language.name === "en");
+      const formName = formData.form_names.find(
         (formName) => formName.language.name === "en"
       );
       return (name ?? formName)?.name || "";
