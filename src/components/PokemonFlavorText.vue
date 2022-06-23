c4b066c7fa53763a650a21f5e27589249817decd
Patrick Sletvold <patricksletvold@hotmail.com>
2022-06-23 16:27:47 +0200

    Rework flavor text component to enable toggling between different game versions
    Including built-in deduplication of identical flavor texts

--- src/components/PokemonFlavorText.vue
+++ src/components/PokemonFlavorText.vue
@@ -1,14 +1,22 @@
 <template>
   <div v-if="flavorTextEntries" class="subtitle">
-    <p>{{ pokemonFlavorTextLocalized }}</p>
+    <o-tabs v-model="gameVersion" multiline>
+      <PokemonFlavorTextItem
+        v-for="version in pokemonVersions"
+        :key="version.value"
+        :versionNames="version.versionNames"
+        :flavorText="pokemonFlavorTextObject[version.value]"
+      />
+    </o-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { usePokemonStore } from "@/stores/pokemonStore";
-import { findWithLanguage } from "@/utils/pokemonUtils";
+import { computed, ref, watch } from "vue";
 import type { FlavorText } from "@16patsle/pokeapi.js";
+import PokemonFlavorTextItem from "./PokemonFlavorTextItem.vue";
+import { usePokemonStore } from "@/stores/pokemonStore";
+import { normalizeTextForComparison } from "@/utils/pokemonUtils";
 
 const props = defineProps<{
   flavorTextEntries: FlavorText[];
@@ -19,17 +27,62 @@ const gameVersion = ref("red");
 const store = usePokemonStore();
 
 const pokemonFlavorTextLocalized = computed(() => {
-  const filteredFlavorTextEntries = props.flavorTextEntries.filter(
-    (text) => text.version.name === gameVersion.value
+  let flavorText = props.flavorTextEntries.filter(
+    (flavorText) => flavorText.language.name === store.language
   );
-  const flavorText = findWithLanguage(
-    filteredFlavorTextEntries,
-    store.language
-  );
-  if (flavorText) {
-    return flavorText.flavor_text;
-  } else {
-    return findWithLanguage(filteredFlavorTextEntries)?.flavor_text ?? "";
+  if (flavorText.length === 0) {
+    flavorText = props.flavorTextEntries.filter(
+      (flavorText) => flavorText.language.name === "en"
+    );
   }
+  return flavorText;
 });
+
+const pokemonFlavorTextObject = computed(() => {
+  const flavorTextObject: { [key: string]: FlavorText } = {};
+  pokemonFlavorTextLocalized.value.forEach((flavorText) => {
+    flavorTextObject[flavorText.version.name] = flavorText;
+  });
+  return flavorTextObject;
+});
+
+const pokemonVersions = computed(() => {
+  type EntryVersion = { versionNames: string[]; flavorText: FlavorText };
+  const entryVersions: EntryVersion[] = [];
+
+  pokemonFlavorTextLocalized.value.forEach((entry) => {
+    const entryFlavorText = normalizeTextForComparison(entry.flavor_text);
+    const previous = entryVersions.find((version) => {
+      const versionFlavorText = normalizeTextForComparison(
+        version.flavorText.flavor_text
+      );
+      return versionFlavorText === entryFlavorText;
+    });
+
+    // If a previous entry with the same flavor text exists, add the version to the list of versions for that entry.
+    if (previous) {
+      previous.versionNames.push(entry.version.name);
+    } else {
+      entryVersions.push({
+        versionNames: [entry.version.name],
+        flavorText: entry,
+      });
+    }
+  });
+
+  return entryVersions.map((entryVersion) => {
+    return {
+      versionNames: entryVersion.versionNames,
+      // Prefer using the second version, since the first one has weird formatting.
+      value: entryVersion.versionNames[1] ?? entryVersion.versionNames[0],
+    };
+  });
+});
+
+const setVersion = ([newVersion]: typeof pokemonVersions.value) => {
+  gameVersion.value = newVersion.versionNames[1] ?? newVersion.versionNames[0];
+};
+
+watch(pokemonVersions, setVersion);
+setVersion(pokemonVersions.value);
 </script>
---
+++ src/components/PokemonFlavorTextItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <o-tab-item
+    v-if="text && versionNames?.length > 0"
+    :value="versionNames[1] ?? versionNames[0]"
+    :label="label"
+  >
+    <p>{{ text }}</p>
+  </o-tab-item>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import type { FlavorText } from "@16patsle/pokeapi.js";
+
+const props = defineProps<{
+  versionNames: string[];
+  flavorText: FlavorText;
+}>();
+
+const label = computed(() => {
+  return props.versionNames.join(", ");
+});
+const text = computed(() => props.flavorText.flavor_text);
+</script>
